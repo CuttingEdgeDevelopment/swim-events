c4735172f049b6735d4329e7f62c2c8db33ea0fb
Nicolas Messer <nme@nicolasmesser.ch>
2024-05-28 17:30:05 +0200

    Improve Project Structure

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ import { format } from "/node_modules/@formkit/tempo/dist/index.mjs";
 
 const date = format(new Date(), "full");
 
-console.log(date);
+let currentDateEl = document.getElementById("currentDate");
+
+currentDateEl.textContent = date;
