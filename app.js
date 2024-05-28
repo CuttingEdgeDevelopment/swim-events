d1f755ad44a4fc85b6045dc6e1975fc477280834
Nicolas Messer <nme@nicolasmesser.ch>
2024-05-28 20:56:54 +0200

    Implementing Event Countdown

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-import { format } from "/node_modules/@formkit/tempo/dist/index.mjs";
+import { format, diffDays } from "/node_modules/@formkit/tempo/dist/index.mjs";
 
 const date = format(new Date(), "full");
-
 let currentDateEl = document.getElementById("currentDate");
+let eventDateEl = document.getElementsByClassName("eventDate");
+let eventCountdownEl = document.getElementsByClassName("eventCountdown");
+let countdown = diffDays("2024-05-29", "2024-05-28");
 
 currentDateEl.textContent = date;
+eventCountdownEl[0].textContent = countdown;
