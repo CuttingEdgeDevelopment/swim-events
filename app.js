2813ee140667b24ebfe5c85f9d62d07499fea6f8
Nicolas Messer <nme@nicolasmesser.ch>
2024-05-28 21:12:16 +0200

    Integrating FormKit Parse Function

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-import { format, diffDays } from "/node_modules/@formkit/tempo/dist/index.mjs";
+import { format, parse, diffDays } from "/node_modules/@formkit/tempo/dist/index.mjs";
 
 const date = format(new Date(), "full");
 let currentDateEl = document.getElementById("currentDate");
 let eventDateEl = document.getElementsByClassName("eventDate");
+let eventDate = parse(eventDateEl[0].textContent, "DD.MM.YYYY")
 let eventCountdownEl = document.getElementsByClassName("eventCountdown");
-let countdown = diffDays("2024-05-29", "2024-05-28");
+let countdown = diffDays(eventDate, "2024-05-28");
 
 currentDateEl.textContent = date;
 eventCountdownEl[0].textContent = countdown;
