a6b5ae849e36f7e0f24fa29220623a59f89b8d8c
Nicolas Messer <nme@nicolasmesser.ch>
2024-05-30 15:43:22 +0200

    Setting up Data Pipeline from JSON

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 // import { format, parse, diffDays } from "/node_modules/@formkit/tempo/dist/index.mjs";
 import { format, parse, diffDays } from "https://cdn.jsdelivr.net/npm/@formkit/tempo/+esm";
+import eventsList from "/src/data/events.json" with { type: 'json' };
 
 const date = format(new Date(), "MMM DD, YYYY");
 const day = format(new Date(), "dddd");
+let events = eventsList.events;
 let currentDateEl = document.getElementById("currentDate");
 let currentDayEl = document.getElementById("currentDay");
 let currentDate = parse(date, "dddd DD MMMM YYYY");
@@ -10,6 +12,11 @@ let eventDateEl = document.getElementsByClassName("eventDate");
 let eventDate = parse(eventDateEl[0].textContent, "DD.MM.YYYY")
 let eventCountdownEl = document.getElementsByClassName("eventCountdown");
 let countdown = diffDays(eventDate, currentDate);
+
+events.forEach(event => {
+  console.log(event.name);
+  console.log(event.dateStart);
+});
 
 currentDateEl.textContent = "Current Date: " + date;
 currentDayEl.textContent = "Happy " + day + "!";
@@ -21,10 +28,19 @@ if (countdown === 0) {
   eventCountdownEl[0].textContent = countdown + " days to go!";
 }
 
-function readJSON() {
-  fetch('./src/data/events.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+/* Initial fetch function
+-------------------------
+function fetchJSONData() {
+  fetch("./src/data/events.json")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      return res.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Unable to fetch data:", error));
 }
 
-readJSON();
+fetchJSONData();
+*/
