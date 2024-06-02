6fa534a158b9c033f25f75e32e08e2f06a72aeec
Nicolas Messer <nme@nicolasmesser.ch>
2024-06-02 18:07:39 +0200

    Implementing Load & Update Functionality

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ import eventsList from "/src/data/events.json" with { type: 'json' };
 
 const date = format(new Date(), "MMM DD, YYYY");
 const day = format(new Date(), "dddd");
+const btnAdd = document.getElementById("btn-add");
+const btnUpdate = document.getElementById("btn-update");
 let events = eventsList.events;
 let eventsSection = "";
 let currentDateEl = document.getElementById("currentDate");
@@ -15,40 +17,38 @@ let eventCountdownEl = document.getElementsByClassName("eventCountdown");
 currentDateEl.textContent = "Current Date: " + date;
 currentDayEl.textContent = "Happy " + day + "!";
 
-events.forEach(event => {
-  let eventDate = parse(event.dateStart, "DD.MM.YYYY")
-  let countdown = diffDays(eventDate, currentDate);
-  eventsSection = '<section class="event">' +
-    '<h4>' + event.name + '</h4>' +
-    '<p>Date: <span class="eventDate">' + event.dateStart + '</span></p>';
+function listEvents() {
+  events.forEach(event => {
+    let eventDate = parse(event.dateStart, "DD.MM.YYYY")
+    let countdown = diffDays(eventDate, currentDate);
+    eventsSection = '<section class="event">' +
+      '<h4>' + event.name + '</h4>' +
+      '<p>Date: <span class="eventDate">' + event.dateStart + '</span></p>';
 
-  if (countdown === 0) {
-    eventCountdownEl.textContent = "Today is the day!";
-    eventsSection += '<p>Countdown: <span class="eventCountdown">' + eventCountdownEl.textContent + '</span></p>' + '</section>';
-  } else if (countdown === 1) {
-    eventCountdownEl.textContent = countdown + " day to go!";
-    eventsSection += '<p>Countdown: <span class="eventCountdown">' + eventCountdownEl.textContent + '</span></p>' + '</section>';
-  } else {
-    eventCountdownEl.textContent = countdown + " days to go!";
-    eventsSection += '<p>Countdown: <span class="eventCountdown">' + eventCountdownEl.textContent + '</span></p>' + '</section>';
-  }
+    if (countdown === 0) {
+      eventCountdownEl.textContent = "Today is the day!";
+      eventsSection += '<p>Countdown: <span class="eventCountdown">' + eventCountdownEl.textContent + '</span></p>' + '</section>';
+    } else if (countdown === 1) {
+      eventCountdownEl.textContent = countdown + " day to go!";
+      eventsSection += '<p>Countdown: <span class="eventCountdown">' + eventCountdownEl.textContent + '</span></p>' + '</section>';
+    } else {
+      eventCountdownEl.textContent = countdown + " days to go!";
+      eventsSection += '<p>Countdown: <span class="eventCountdown">' + eventCountdownEl.textContent + '</span></p>' + '</section>';
+    }
 
-  eventsListEl.insertAdjacentHTML("beforeend", eventsSection);
-});
-
-/* Initial fetch function
--------------------------
-function fetchJSONData() {
-  fetch("./src/data/events.json")
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error(`HTTP error! Status: ${res.status}`);
-      }
-      return res.json();
-    })
-    .then((data) => console.log(data))
-    .catch((error) => console.error("Unable to fetch data:", error));
+    eventsListEl.insertAdjacentHTML("beforeend", eventsSection);
+  });
 }
 
-fetchJSONData();
-*/
+window.addEventListener("load", event => {
+  listEvents();
+});
+
+btnAdd.addEventListener("click", event => {
+  console.log("This button will add a new event");
+});
+
+btnUpdate.addEventListener("click", event => {
+  eventsListEl.textContent = "";
+  listEvents();
+});
