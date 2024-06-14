2287f91abc18497c043cc133f7fba8c680068f47
Nicolas Messer <nme@nicolasmesser.ch>
2024-06-14 12:39:10 +0200

    Improving Design

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,6 @@ let currentDateEl = document.getElementById("currentDate");
 let currentDayEl = document.getElementById("currentDay");
 let eventsListEl = document.getElementById("eventsList");
 let currentDate = parse(date, "dddd DD MMMM YYYY");
-let eventCountdownEl = document.getElementsByClassName("eventCountdown");
 let eventsLoadingEl = document.getElementById("eventsLoading");
 
 currentDayEl.textContent = "Happy " + day + "!";
@@ -20,35 +19,34 @@ currentDateEl.textContent = "Current Date: " + date;
 
 function listEvents() {
   data.events.forEach(event => {
-    let eventStartDate = parse(event.dateStart, "DD.MM.YYYY")
-    let eventEndDate = parse(event.dateEnd, "DD.MM.YYYY")
+    let eventStartDate = parse(event.dateStart, "DD.MM.YYYY");
+    let eventEndDate = parse(event.dateEnd, "DD.MM.YYYY");
+    let startDate = format(eventStartDate, "dddd, MMMM DD, YYYY");
+    let endDate = format(eventEndDate, "dddd, MMMM DD, YYYY");
     let countdown = diffDays(eventStartDate, currentDate);
     let daysPassed = diffDays(eventEndDate, currentDate);
 
     if (countdown < 0) {
       console.log(event.name + " ended " + daysPassed + " days ago.");
     } else {
-      eventsSection = '<section class="event event-' + event.id + ' border border-black rounded-xl mb-3 px-4 py-5">';
+      eventsSection = '<section class="event event-' + event.id + ' border border-black rounded-xl mb-3 px-4 py-5 flex justify-between"><div id="eventInfo">';
 
       if (event.link === "") {
-        eventsSection += '<h4 class="mb-2 text-xl">' + event.name + '</h4>';
+        eventsSection += '<h4 class="mb-2 text-xl font-normal">' + event.name + '</h4>';
       } else {
-        eventsSection += '<h4 class="mb-2 text-xl"><a href="' + event.link + '" title="' + event.name + '" target="_blank">' + event.name + '</a></h4>';
+        eventsSection += '<h4 class="mb-2 text-xl font-normal hover:font-semibold"><a href="' + event.link + '" title="' + event.name + '" target="_blank">' + event.name + '</a></h4>';
       }
 
-      eventsSection += '<p>Location: ' + event.location + '</p>' +
-        '<p>Begin: ' + event.dateStart + '</p>' +
-        '<p>End: ' + event.dateEnd + '</p>';
+      eventsSection += '<p><span class="font-medium">Location:</span> ' + event.location + '</p>' +
+        '<p><span class="font-medium">Begin:</span> ' + startDate + '</p>' +
+        '<p><span class="font-medium">End:</span> ' + endDate + '</p></div><div id="eventCountdown" class="text-center">';
 
       if (countdown === 0) {
-        eventCountdownEl.textContent = "Today is the day!";
-        eventsSection += '<p>Countdown: <span class="eventCountdown">' + eventCountdownEl.textContent + '</span></p>' + '</section>';
+        eventsSection += '<p class="font-bold">Today is the day!</p></div></section>';
       } else if (countdown === 1) {
-        eventCountdownEl.textContent = countdown + " day to go";
-        eventsSection += '<p>Countdown: <span class="eventCountdown">' + eventCountdownEl.textContent + '</span></p>' + '</section>';
+        eventsSection += '<p class="text-7xl">' + countdown + '</p><p class="font-bold">day to go</p>' + '</div></section>';
       } else {
-        eventCountdownEl.textContent = countdown + " days to go";
-        eventsSection += '<p>Countdown: <span class="font-bold">' + eventCountdownEl.textContent + '</span></p>' + '</section>';
+        eventsSection += '<p class="text-7xl">' + countdown + '</p><p class="font-bold">days to go</p>' + '</div></section>';
       }
     }
     eventsListEl.insertAdjacentHTML("beforeend", eventsSection);
