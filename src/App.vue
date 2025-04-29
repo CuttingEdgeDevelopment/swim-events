3f7a14a8776edb5d71043fab3a5d327178e53c1d
Nicolas Messer <nme@nicolasmesser.ch>
2025-04-29 16:09:19 +0200

    Continue Transition to Vue Framework

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <Header name="Swim Events Countdown" />
-  <main class="px-16 py-8">
-    <div><p>Main Content</p></div>
-  </main>
+  <Header :name="name" :currentDay="day" :currentDate="date" />
+  <Main />
   <Footer />
 </template>
 
 <script setup>
   import Header from "./Header.vue";
+  import Main from "./Main.vue";
   import Footer from "./Footer.vue";
 
+  import { format, parse, diffDays, isAfter } from "@formkit/tempo";
+  import { LinkIcon, RocketLaunchIcon, FireIcon } from '@heroicons/vue/24/outline'
+  import { TabGroup, TabList, Tab, TabPanels, TabPanel } from '@headlessui/vue'
+  import { supabase } from "./lib/supabaseClient";
+
+  const name = "Swim Events Countdown";
+  const date = format(new Date(), "MMMM DD, YYYY");
+  const day = format(new Date(), "dddd");
 </script>
